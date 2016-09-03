19a4714737435acdfca32be95112ceb0662a243d
alicetragedy <girl@alicetragedy.org>
2016-09-03 15:38:59 +0200

    update small script to compare against values from our variables and return job

--- file.py
+++ file.py
@@ -11,16 +11,20 @@ with open('data.csv', 'rb') as csvfile:
       single_job.append(column)
     all_jobs.append(row)
   # example: print first item (job title) for second row (first job)
-  print(all_jobs[1][0])
+  #print(all_jobs[1][0])
   
   saved_jobs = []
   saved_job = []
 
   for each_item in all_jobs:
-    if str(userlocation) in str(each_item):
-      print("hooray!")
+    if str(userlocation) in each_item[1]:
+      if str(useredu) in each_item[3]:
+        if str(userfield) in each_item[2]:
+          print(each_item[0] + "\n" + each_item[-1])
+        else:
+          continue
     else:
-      print("whatever")
+      continue
 
 
     #print '\n'.join(row)
